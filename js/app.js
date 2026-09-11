fcf96258b735721d6b65579ef26e25b22690cdb0
yorchAdalbert <george_nlc@hotmail.com>
2026-09-11 06:50:07 -0600

    Add dynamic ingredient cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 const dish = menu[0];
 
-const ingredientList = document.querySelector("#ingredient-list");
+const ingredientList = document.querySelector("#ingredients-list");
 
 dish.ingredients.forEach((ingredient) => {
-  console.log(ingredient.name);
+  const card = document.createElement("div");
+
+  card.className = "ingredient-card";
+
+  card.innerHTML = `
+    <img src="${ingredient.icon}" alt="${ingredient.name}" />
+    <p>${ingredient.name}</p>
+  `;
+
+  ingredientList.appendChild(card);
 });
