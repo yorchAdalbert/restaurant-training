132c9b78ce5b575c20bf639876f017b8af5471ed
yorchAdalbert <george_nlc@hotmail.com>
2026-09-12 19:25:57 -0600

    Complete Tuna Toast ingredint section

--- js/menu-data.js
+++ js/menu-data.js
@@ -8,20 +8,45 @@ const menu = [
 
     ingredients: [
       {
+        name: "Sourdough Batard Bread",
+        icon: "icons/ingredients/sourdough-batard.png",
+        description: "A slice of sourdough batard bread.",
+      },
+      {
         name: "Fresh Tuna",
-        icon: "icons/ingredients/fresh-tuna.svg",
-        description: "Fresh tuna cut into small cubes for the toast."
+        icon: "icons/ingredients/fresh-tuna.png",
+        description: "Fresh tuna used as the main ingredient of the dish.",
       },
       {
         name: "Chipotle Mayo",
-        icon: "icons/ingredients/chipotle-mayo.svg",
-        description: "Creamy mayonnaise with a smoky and mildly spicy chipotle flavor."
+        icon: "icons/ingredients/chipotle-mayo.png",
+        description: "Creamy mayonnaise flavored with chipotle.",
       },
       {
-        name: "Avocado",
-        icon: "icons/ingredients/avocado.svg",
-        description: "Fresh avocado that adds creaminess and balances the spicy elements."
-      }
-    ]
-  }
+        name: "Sriracha Sauce",
+        icon: "icons/ingredients/sriracha.png",
+        description: "Chili sauce that adds heat to the dish.",
+      },
+      {
+        name: "Diced Avocado",
+        icon: "icons/ingredients/avocado.png",
+        description: "Fresh avocado cut into cubes.",
+      },
+      {
+        name: "Persian Cucumber",
+        icon: "icons/ingredients/persian-cucumber.png",
+        description: "Persian cucumber that adds freshness and crunch.",
+      },
+      {
+        name: "Green Onion",
+        icon: "icons/ingredients/green-onion.png",
+        description: "Fresh green onion used to add a mild onion flavor.",
+      },
+      {
+        name: "Cilantro",
+        icon: "icons/ingredients/cilantro.png",
+        description: "Fresh cilantro used as an aromatic herb.",
+      },
+    ],
+  },
 ];
